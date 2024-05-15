fa991ebbac68c13112a56695945648550b97d225
HuMaNus@s <73073973+HuMaNus2s@users.noreply.github.com>
2024-05-15 13:17:57 +0300

    Update NeiroFoodBot.py

--- NeiroFoodBot.py
+++ NeiroFoodBot.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 import logging
 import pytz
 
+# Загрузка конфигурации
 file_for_config = open('config.json', 'r')
 config = json.load(file_for_config)
 prefix = config['prefix']
@@ -28,10 +29,11 @@ log_file = "logs/NeiroFoodBot_{}.log".format(log_time)
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s][%(name)s][%(levelname)s] - %(message)s',
                     filename=log_file, # Имя файла для логов
-                    filemode='w') # Режим записи/создание файла - добавление новых записей в конец файла
+                    filemode='w')
 
+# Создание бота
 bot = telebot.TeleBot(config['token'])
-#================================================================================= Неизменяемые функции
+
 def printt(info):
     now = datetime.now(tz=moscow_tz)
     current_time = now.strftime(f"%Y-%m-%d %H:%M:%S")
@@ -40,7 +42,7 @@ def printt(info):
 def on_ready():
     printt(f"BOT ACTIVATED!")
     logger.info('BOT ACTIVATED!')
-#================================================================================= Записи логов
+
 def log(message=None, error=False, button=None):
     if not error and not button: # Лог использования команды
         printt(f'{message.from_user.username} использовал команду <{message.text}>')
@@ -51,7 +53,15 @@ def log(message=None, error=False, button=None):
     else: # Лог ошибки использования команды
         printt(f'Неизвестная команда <{message.text}>')
         logger.info(f'Неизвестная команда <{message.text}>')
-#================================================================================= Команды
+
+# Функция для создания клавиатуры с кнопкой "Назад"
+def create_back_button():
+    keyboard = types.InlineKeyboardMarkup()
+    button_back = types.InlineKeyboardButton("Назад", callback_data='back')
+    keyboard.row(button_back)
+    return keyboard
+
+# Обработка команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     try:
@@ -60,64 +70,48 @@ def handle_start(message):
         button_FAQ = types.InlineKeyboardButton("FAQ", callback_data='FAQ')
         keyboard.row(button_about, button_FAQ)
         photo_path = 'img/LogoNeiroFood.jpg'
-        bot.send_photo(message.chat.id, photo=open(photo_path, 'rb'), caption='Привет! Я ваш личный ассистент NeiroFood!\n Меню:', reply_markup=keyboard)
+        bot.send_photo(message.chat.id, photo=open(photo_path, 'rb'), caption='Привет! Я ваш личный ассистент NeiroFood!\n', reply_markup=keyboard)
         log(message)
     except Exception as e:
         printt(f"Ошибка при отправке изображения: {e}")
         logger.info(f'{e}')
 
-
 @bot.message_handler(commands=['help'])
 def handle_help(message):
     bot.reply_to(message, f"Список доступных команд:\n\n{command_name(0)} - приветствие\n{command_name(1)} - справка")
     log(message)
-#================================================================================= Обработка кнопок
+
+# Обработка нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
-    message = call.message
-    if call.data == 'about': # О нас
+    if call.data == 'about':
         text = '''
 *Проект NeiroFood 🍔*
 
 Привет! Мы - студенты, создали проект *NeiroFood* в рамках курса Интернет-Маркетинга. Это бургерная, где мы используем нейросети для создания уникальных рецептов бургеров. Приходите попробовать! 🍔✨'''
-        bot.send_message(message.chat.id, text, parse_mode="Markdown")
-        log(message, False, call.data)
-    elif call.data == 'FAQ': # FAQ
+        photo_path = 'img/AboutNeiroFood.jpg'
+        media = types.InputMediaPhoto(open(photo_path, "rb"), caption=text, parse_mode="Markdown")
+        bot.edit_message_media(media , call.message.chat.id, call.message.id, reply_markup=create_back_button())
+    elif call.data == 'FAQ':
         text = '''*1. Что такое "NeiroFood"?*
 *NeiroFood* - это бургерная 🍔, созданная студентами в рамках курса Интернет-Маркетинга. Мы используем нейросети для создания уникальных рецептов бургеров.
 
-*2. Как я могу ознакомиться с вашим меню?*
-Меню доступно в нашем телеграм-боте. Просто нажмите кнопку "Меню" для просмотра.
-
-*3. Как я могу сделать заказ?*
+*2. Как я могу сделать заказ?*
 Для оформления заказа выберите бургер из меню и следуйте инструкциям нашего бота. Мы доставим ваш заказ в указанное место в кратчайшие сроки.
 
-*4. Какие способы оплаты вы принимаете?*
-Мы принимаем оплату наличными и через онлайн-платформы, такие как карты и электронные кошельки.
-
-*5. Есть ли у вас программы лояльности или скидки для постоянных клиентов?*
+*3. Есть ли у вас программы лояльности или скидки для постоянных клиентов?*
 В настоящее время мы еще не предлагаем программы лояльности или акции для постоянных клиентов, но активно работаем над этим. Следите за нашими обновлениями в нашем телеграм-канале или в приложении.
 
-*6. Как я могу связаться с вашей поддержкой в случае возникновения проблем?*
-Для связи с нашей поддержкой вы можете написать нам через телеграм-бота или позвонить по указанному номеру телефона, так же у нас имеется сайт.
+*4. Как я могу связаться с вашей поддержкой в случае возникновения проблем?*
+Для связи с нашей поддержкой вы можете написать нам через телеграм-бота или позвонить по указанному номеру телефона, так же у нас имеется сайт.'''
+        photo_path = 'img/FAQNeiroFood.jpg'
+        media = types.InputMediaPhoto(open(photo_path, "rb"), caption=text, parse_mode="Markdown")
+        bot.edit_message_media(media , call.message.chat.id, call.message.id, reply_markup=create_back_button())
+    elif call.data == 'back':
+        handle_start(call.message)
 
-*7. Какие часы работы у вас?*
-Мы работаем ежедневно с *9:00* до *15:50*.'''
-        bot.send_message(message.chat.id, text,parse_mode="Markdown")
-        log(message, False, call.data)
 
-#================================================================================= Текстовые команды
-@bot.message_handler(func=lambda message: True)
-def handle_text_messages(message):
-    if message.text == 'Привет':
-        bot.reply_to(message, f"Приветствую! Рад тебя видеть!")
-    else:
-        send_unknown_command_message(message)
 
-def send_unknown_command_message(message):
-    bot.reply_to(message, f"Извините, я не понимаю эту команду. Попробуйте {command_name(1)}.")
-    log(message, True)
-#=================================================================================
 if __name__ == '__main__':
     try:
         on_ready()
